71a8e5651aaa69b85c19afc55d48b9efca388559
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2023-11-10 11:57:51 +0100

    commit1

---
+++ assets/JS/backOffice.js
@@ -0,0 +1,3 @@
+const url = "https://striveschool-api.herokuapp.com/api/product";
+const apiKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZjAxMDI1NGU4ODAwMTgzZjE4YTQiLCJpYXQiOjE2OTk2MDY1NDQsImV4cCI6MTcwMDgxNjE0NH0.0qI6ZXIBq7k15gJ4m71fqgm--AV4OOkFF4FI_NLQVxI";
